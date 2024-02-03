71be90b4ccc674939b1d738d4e904b1f0b0811c4
Cengizhan Akgün <coderista@gmail.com>
2024-02-04 02:24:36 +0300

    Add project files.

---
+++ CompressJPEG/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CompressJPEG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResize
+            // 
+            this.btnResize.Location = new System.Drawing.Point(10, 12);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(266, 23);
+            this.btnResize.TabIndex = 0;
+            this.btnResize.Text = "Select JPEG File";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            // 
+            // numericUpDownHeight
+            // 
+            this.numericUpDownHeight.Location = new System.Drawing.Point(156, 41);
+            this.numericUpDownHeight.Maximum = new decimal(new int[] {
+            1080,
+            0,
+            0,
+            0});
+            this.numericUpDownHeight.Name = "numericUpDownHeight";
+            this.numericUpDownHeight.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownHeight.TabIndex = 4;
+            this.numericUpDownHeight.Value = new decimal(new int[] {
+            768,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.Location = new System.Drawing.Point(10, 41);
+            this.numericUpDownWidth.Maximum = new decimal(new int[] {
+            1920,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownWidth.TabIndex = 5;
+            this.numericUpDownWidth.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 85);
+            this.Controls.Add(this.numericUpDownWidth);
+            this.Controls.Add(this.numericUpDownHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResize);
+            this.Name = "Form1";
+            this.Text = "Compress JPEG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownHeight;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+    }
+}
+
---
+++ CompressJPEG/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Encoder = System.Drawing.Imaging.Encoder;
+
+namespace CompressJPEG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPEG Files|*.jpg;*.jpeg";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string outputPath = folderBrowserDialog.SelectedPath;
+                    ResizeImage(filePath, outputPath);
+                }
+            }
+        }
+
+        private void ResizeImage(string filePath, string outputPath)
+        {
+            // Yeni boyutu al
+            int newWidth = Convert.ToInt32(numericUpDownWidth.Value);
+            int newHeight = Convert.ToInt32(numericUpDownHeight.Value);
+
+            using (Image oldImage = Image.FromFile(filePath))
+            {
+                using (Bitmap newImage = new Bitmap(newWidth, newHeight))
+                {
+                    using (Graphics g = Graphics.FromImage(newImage))
+                    {
+                        g.DrawImage(oldImage, new Rectangle(0, 0, newWidth, newHeight));
+                    }
+
+                    // JPEG sıkıştırma kalitesi
+                    EncoderParameter qualityParam = new EncoderParameter(Encoder.Quality, 100L);
+                    ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+                    if (jpegCodec != null)
+                    {
+                        EncoderParameters encoderParams = new EncoderParameters(1);
+                        encoderParams.Param[0] = qualityParam;
+
+                        // Yeni dosyayı belirtilen hedefe kaydet
+                        string fileName = Path.GetFileName(filePath);
+                        string outputPathWithFileName = Path.Combine(outputPath, fileName);
+                        newImage.Save(outputPathWithFileName, jpegCodec, encoderParams);
+                        MessageBox.Show("Resized and saved successfully.");
+                    }
+                }
+            }
+        }
+
+        private ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.MimeType == mimeType)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+
+
+}
